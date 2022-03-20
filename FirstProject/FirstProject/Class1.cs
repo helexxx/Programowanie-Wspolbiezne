a23beaa61ad548eca905f4a14cd2c400c154f9a6
helexxx <wojtek.stankiewicz1997@gmail.com>
2022-03-20 12:56:33 +0100

    Implementation

--- FirstProject/FirstProject/Class1.cs
+++ FirstProject/FirstProject/Class1.cs
@@ -4,10 +4,28 @@ namespace FirstProject
 {
     public class Calculator
     {
- 
-        public int Add(int v1, int v2)
+       public int Add(int v1, int v2)
         {
             return v1 + v2;
+        }
+        public int Sub(int v1, int v2)
+        {
+            return v1- v2;
+        }
+        public int Multi(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+        public double Div(int v1, int v2)
+        {
+            if(v2 != 0 )
+            {
+                return v1 / v2;
+            }
+            else
+            {
+                throw new ArgumentException("Division by zero");
+            }
         }
     }
 }
