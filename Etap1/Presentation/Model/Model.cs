a314981b5e97f17b5c9b8e93c0d391da6235cfaf
helexxx <wojtek.stankiewicz1997@gmail.com>
2022-04-15 22:17:54 +0200

    Presentation- data for gui, including logic layer

---
+++ Etap1/Presentation/Model/Model.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Model
+{
+    public class Model
+    {
+        public static void Main()
+        {
+            Logic.LogicAPI logicLayer = Logic.LogicAPI.CreateAPI(Data.DataAPI.CreateAPI());
+            Logic.Box box = logicLayer.CreateBox(100, 100, 3, 3);
+
+            for (int i = 0; i < box.Balls.Count; i++)
+            {
+                System.Console.WriteLine("Ball " + i.ToString());
+                System.Console.WriteLine(box.Balls[i].get_x);
+                System.Console.WriteLine(box.Balls[i].get_y);
+                System.Console.WriteLine(box.Balls[i].get_radius);
+            }
+
+        }
+    }
+}
