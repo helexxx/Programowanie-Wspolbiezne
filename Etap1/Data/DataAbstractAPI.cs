e2f9fd409c2b9e4bf7ec271146a0b2686022321c
helexxx <wojtek.stankiewicz1997@gmail.com>
2022-05-09 15:05:59 +0200

    data empty implementation

---
+++ Etap1/Data/DataAbstractAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public abstract class DataAbstractAPI
+    {
+        public static DataAbstractAPI CreateDataAPI()
+        {
+            return new DataLayer();
+        }
+
+    }
+    internal class DataLayer : DataAbstractAPI
+    {
+        public DataLayer() { }
+    }
+}
