8319c2cb2055069564031bf4974efae7cbfe07cf
helexxx <wojtek.stankiewicz1997@gmail.com>
2022-05-10 16:43:19 +0200

    etap 1 working

---
+++ Etap2/ViewModel/MainWindowViewModel .cs
@@ -0,0 +1,74 @@
+﻿using Logic;
+using Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using ViewModel;
+
+namespace MainWindowViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+
+    {
+        #region public API
+
+        public MainWindowViewModel() : this(ModelAbstractApi.CreateApi())
+        {
+        }
+
+        public MainWindowViewModel(ModelAbstractApi modelAbstractApi)
+        {
+            ModelLayer = modelAbstractApi;
+            Start = new RelayCommand(() => StartHandler());
+            Stop = new RelayCommand(() => StopHandler());
+        }
+
+        public int Count
+        {
+            get
+            {
+                return ModelLayer.Count;
+            }
+            set
+            {
+                if (value.Equals(ModelLayer.Count))
+                    return;
+                ModelLayer.Count = value;
+                RaisePropertyChanged("Count");
+            }
+        }
+
+        public ObservableCollection<Ball> Ball
+        {
+            get
+            {
+                return ModelLayer.Balls;
+            }
+        }
+
+        public ICommand Start { get; set; }
+        public ICommand Stop { get; set; }
+
+        private void StartHandler()
+        {
+            ModelLayer.ModelStartSimulation();
+        }
+
+        private void StopHandler()
+        {
+            ModelLayer.ModelStopSimulation();
+        }
+
+        #endregion public API
+
+        #region private
+
+        private ModelAbstractApi ModelLayer = ModelAbstractApi.CreateApi();
+
+        #endregion private
+
+    }
+}
